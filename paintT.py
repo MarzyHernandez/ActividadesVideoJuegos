597c835809c3eed28ed863c976a3ebe77cd58abd
Marzy Hernandez <marzyhernandez@MacBook-Air-de-Marzy-2.local>
2023-05-10 20:38:56 -0600

    Paint Comentariado

--- paintT.py
+++ paintT.py
@@ -1,19 +1,8 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
-"""
-
 from turtle import *
-
 from freegames import vector
 
-
+# Función para dibujar una línea desde el punto de inicio hasta el punto 
+final.
 def line(start, end):
     """Draw line from start to end."""
     up()
@@ -21,7 +10,8 @@ def line(start, end):
     down()
     goto(end.x, end.y)
 
-
+# Función para dibujar un cuadrado desde el punto de inicio hasta el punto 
+final.
 def square(start, end):
     """Draw square from start to end."""
     up()
@@ -35,11 +25,12 @@ def square(start, end):
 
     end_fill()
 
-
+# Función para dibujar un círculo desde el punto de inicio hasta el punto 
+final.
 def circulo(start, end):
     """Draw circle from start to end."""
     up()
-    radius=abs(start - end)
+    radius = abs(start - end)
     goto(start.x, start.y - radius)
     down()
     fillcolor('royal blue')
@@ -47,32 +38,39 @@ def circulo(start, end):
     circle(radius)
     end_fill()
 
-
+# Función para dibujar un rectángulo desde el punto de inicio hasta el 
+punto final.
 def rectangle(start, end):
     """Draw rectangle from start to end."""
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
+    
     for count in range(2):
         forward(end.x - start.x)
         left(90)
         forward(end.y - start.y)
         left(90)
+    
     end_fill()
 
+# Función para dibujar un triángulo desde el punto de inicio hasta el 
+punto final.
 def triangle(start, end):
     """Draw triangle from start to end."""
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
+
     for count in range(3):
         forward(end.x - start.x)
         left(120)
+    
     end_fill()
 
-
+# Función que se llama cuando se hace clic en la pantalla.
 def tap(x, y):
     """Store starting point or draw shape."""
     start = state['start']
@@ -85,13 +83,14 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
+# Función para almacenar un valor en el estado (diccionario) en una clave 
+específica.
 def store(key, value):
     """Store value in state at key."""
     state[key] = value
 
-
 state = {'start': None, 'shape': line}
+
 setup(420, 420, 370, 0)  # Configura el tamaño y la posición de la ventana 
 de dibujo
 onscreenclick(tap)  # Llama a la función 'tap' cuando se hace clic en la 
@@ -99,6 +98,7 @@ pantalla
 listen()  # Escucha los eventos del teclado
 onkey(undo, 'u')  # Llama a la función 'undo' cuando se presiona la tecla 
 'u'
+
 # Configuramos una serie de funciones lambda para cambiar el color del 
 trazo cuando se presionen ciertas teclas
 onkey(lambda: color('black'), 'K')
@@ -122,6 +122,5 @@ onkey(lambda: store('shape', circulo), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 
-# Finalizamos la configuración y ejecución del programa
-done()
+done()  # Finalizamos
 
