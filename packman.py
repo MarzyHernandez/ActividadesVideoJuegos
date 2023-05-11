21d69e107ef4d0fbad665208dc7682d24eab26c9
Marzy Hernandez <marzyhernandez@MacBook-Air-de-Marzy-2.local>
2023-05-11 13:16:05 -0600

    código comentarizado pacman

--- packman.py
+++ packman.py
@@ -1,19 +1,25 @@
+# Importar librerías necesarias
 from random import choice
 from turtle import *
 from freegames import floor, vector
 
+# Estado del juego
 state = {'score': 0}
-path = Turtle(visible=False)
-writer = Turtle(visible=False)
-aim = vector(5, 0)
-pacman = vector(-40, -80)
+# Inicialización de los elementos del juego
+path = Turtle(visible=False)  # Camino que sigue el personaje
+writer = Turtle(visible=False)  # Texto para mostrar la puntuación
+aim = vector(5, 0)  # Dirección inicial de Pacman
+pacman = vector(-40, -80)  # Posición inicial de Pacman
 ghosts = [
-    [vector(-180, 160), vector(15, 0)],
-    [vector(-180, -160), vector(0, 15)],
-    [vector(100, 160), vector(0, -15)],
-    [vector(100, -160), vector(-15, 0)],
+    [vector(-180, 160), vector(5, 0)],  # Fantasma 1: posición y dirección
+    [vector(-180, -160), vector(0, 5)],  # Fantasma 2: posición y 
+dirección
+    [vector(100, 160), vector(0, -5)],  # Fantasma 3: posición y dirección
+    [vector(100, -160), vector(-5, 0)],  # Fantasma 4: posición y 
+dirección
 ]
 tiles = [
+    # Diseño del laberinto (0: pared, 1: camino)
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0,
     0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0,
     0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0,
@@ -38,38 +44,43 @@ tiles = [
 
 def distance(p1, p2):
     "Return the Manhattan distance between two points."
-    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y) 
 
 def square(x, y):
     "Draw square using path at (x, y)."
-    path.up()
-    path.goto(x, y)
-    path.down()
-    path.begin_fill()
+    path.up()   # Levanta el lápiz
+    path.goto(x, y)   # Mueve el lápiz a las coordenadas (x, y)
+    path.down()   # Baja el lápiz para dibujar
+    path.begin_fill()   # Inicia el relleno
 
     for count in range(4):
-        path.forward(20)
-        path.left(90)
+        path.forward(20)   # Avanza 20 unidades hacia adelante
+        path.left(90)   # Gira 90 grados a la izquierda
 
-    path.end_fill()
+    path.end_fill()   # Finaliza el relleno
 
 def offset(point):
     "Return offset of point in tiles."
-    x = (floor(point.x, 20) + 200) / 20
-    y = (180 - floor(point.y, 20)) / 20
-    index = int(x + y * 20)
+    x = (floor(point.x, 20) + 200) / 20   # Calcula la coordenada x en la 
+matriz de casillas
+    y = (180 - floor(point.y, 20)) / 20   # Calcula la coordenada y en la 
+matriz de casillas
+    index = int(x + y * 20)   # Calcula el índice correspondiente en la 
+matriz de casillas
     return index
 
 def valid(point):
     "Return True if point is valid in tiles."
     index = offset(point)
 
-    if tiles[index] == 0:
+    if tiles[index] == 0: # Valor en el índice de la matriz de casillas es 
+0
         return False
 
     index = offset(point + 19)
 
-    if tiles[index] == 0:
+    if tiles[index] == 0: # Valor en el índice de la matriz de casillas es 
+0
         return False
 
     return point.x % 20 == 0 or point.y % 20 == 0
@@ -83,8 +94,11 @@ def world():
         tile = tiles[index]
 
         if tile > 0:
-            x = (index % 20) * 20 - 200
-            y = 180 - (index // 20) * 20
+            x = (index % 20) * 20 - 200 # Calcula la coordenada x del 
+cuadrado en el mundo
+            y = 180 - (index // 20) * 20# Calcula la coordenada y del 
+cuadrado en el mundo
+
             square(x, y)
 
             if tile == 1:
@@ -94,57 +108,70 @@ def world():
 
 def move():
     "Move pacman and all ghosts."
-    writer.undo()
-    writer.write(state['score'])
+    writer.undo() # Borra el marcador de puntuación
+    writer.write(state['score']) # Escribe la puntuación actual en el 
+marcador
 
-    clear()
+    clear() # Limpia la pantalla
 
-    if valid(pacman + aim):
-        pacman.move(aim)
+    if valid(pacman + aim):   # Si el siguiente movimiento del pacman es 
+válido
+        pacman.move(aim)   # Mueve al pacman en la dirección actual
 
-    index = offset(pacman)
+    index = offset(pacman)   # Obtiene el índice correspondiente al punto 
+del pacman
 
-    if tiles[index] == 1:
-        tiles[index] = 2
-        state['score'] += 1
-        x = (index % 20) * 20 - 200
-        y = 180 - (index // 20) * 20
-        square(x, y)
+    if tiles[index] == 1:   
+        tiles[index] = 2  
+        state['score'] += 1   # Incrementa la puntuación en 1
+        x = (index % 20) * 20 - 200   # Calcula la coordenada x del 
+cuadrado en el mundo
+        y = 180 - (index // 20) * 20   # Calcula la coordenada y del 
+cuadrado en el mundo
+        square(x, y)  
 
     up()
-    goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')
+    goto(pacman.x + 10, pacman.y + 10)   
+    dot(20, 'yellow')  
 
     for ghost in ghosts:
         point, course = ghost
-        if valid(point + course):
-            point.move(course)
+        if valid(point + course):   # Si el siguiente movimiento del 
+fantasma es válido
+            point.move(course)   # Mueve al fantasma en la dirección 
+actual
         else:
             options = [
-                vector(15, 0),
-                vector(-15, 0),
-                vector(0, 15),
-                vector(0, -15),
+                vector(5, 0),
+                vector(-5, 0),
+                vector(0, 5),
+                vector(0, -5),
             ]
             best_option = None
             min_distance = float('inf')
 
             for option in options:
-                if valid(point + option):
-                    target = pacman
-                    dist = abs(target - (point + option))
+                if valid(point + option):   # Si el movimiento de opción 
+es válido
+                    target = pacman   # Establece al pacman como el 
+objetivo
+                    dist = abs(target - (point + option))   # Calcula la 
+distancia al objetivo
 
                     if dist < min_distance:
                         min_distance = dist
                         best_option = option
 
-            if best_option is not None:
-                course.x = best_option.x
-                course.y = best_option.y
+            if best_option is not None:   # Si se encontró una mejor 
+opción de movimiento
+                course.x = best_option.x   # Actualiza la dirección x del 
+fantasma
+                course.y = best_option.y   # Actualiza la dirección y del 
+fantasma
 
         up()
-        goto(point.x + 10, point.y + 10)
-        dot(20, 'red')
+        goto(point.x + 10, point.y + 10)  
+        dot(20, 'red')  
 
     update()
 
@@ -153,7 +180,7 @@ def move():
         if abs(pacman - point) < 20:
             return
 
-    ontimer(move, 100)
+    ontimer(move, 50)
 
 
 
